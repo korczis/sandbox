a8e49778650f1f9c545e13859e2d9bf9e0009b5e
Tomas Korcak <korczis@gmail.com>
2012-01-02 02:13:21 +0100

    DBC & COC changes in program options of TweetStream app

--- TweetStream/app/main.rb
+++ TweetStream/app/main.rb
@@ -6,11 +6,19 @@ require 'yaml'
 require 'Bsl'
 
 class TweetStreamApp < Bsl::Application
+	@@DEFAULT_DB_TYPE = "sql"
+	
 	@@DEFAULT_SQL_HOST = "localhost"
 	@@DEFAULT_SQL_PORT = 5432
 	@@DEFAULT_SQL_DB = "tweetstream"
 	@@DEFAULT_SQL_USER = "host"
 	@@DEFAULT_SQL_PASSWORD = "password"
+	
+	@@DEFAULT_NOSQL_HOST = "localhost"
+	@@DEFAULT_NOSQL_PORT = 5984
+	@@DEFAULT_NOSQL_DB = "tweetstream"
+	@@DEFAULT_NOSQL_USER = "host"
+	@@DEFAULT_NOSQL_PASSWORD = "password"
 	
 	@@DEFAULT_CONSUMER_KEY = "twitter_consumer_key"
 	@@DEFAULT_CONSUMER_SECRET = "twitter_consumer_secret"
@@ -19,46 +27,79 @@ class TweetStreamApp < Bsl::Application
 	
 	def initialize(opts = {})
 		super(opts)
-			
+		
+		options['db_type'] = @@DEFAULT_DB_TYPE
+		
+		options['postgresql_host'] = @@DEFAULT_SQL_HOST
+		options['postgresql_port'] = @@DEFAULT_SQL_PORT
+		options['postgresql_db'] = @@DEFAULT_SQL_DB
+		options['postgresql_user'] = @@DEFAULT_SQL_USER
+		options['postgresql_password'] = @@DEFAULT_SQL_PASSWORD
+		
+		options['couchdb_host'] = @@DEFAULT_NOSQL_HOST
+		options['couchdb_port'] = @@DEFAULT_NOSQL_PORT
+		options['couchdb_db'] = @@DEFAULT_NOSQL_DB
+		options['couchdb_user'] = @@DEFAULT_NOSQL_USER
+		options['couchdb_password'] = @@DEFAULT_NOSQL_PASSWORD
+		
 		options['consumer_key'] = @@DEFAULT_CONSUMER_KEY
 		options['consumer_secret'] = @@DEFAULT_CONSUMER_SECRET
 		options['oauth_token'] = @@DEFAULT_OAUTH_TOKEN
 		options['oauth_token_secret'] = @@DEFAULT_OAUTH_TOKEN_SECRET
-		
-		options['sql_host'] = @@DEFAULT_SQL_HOST
-		options['sql_port'] = @@DEFAULT_SQL_PORT
-		options['sql_db'] = @@DEFAULT_SQL_DB
-		options['sql_user'] = @@DEFAULT_SQL_USER
-		options['sql_password'] = @@DEFAULT_SQL_PASSWORD
 	end
 	
 	def initialize_options(opts = {})
 		super(opts)
 		
-		opts.on('--sql-host STRING', "PostgreSQL Hostname") do |val|
-			options['sql_host'] = val
+		# DB Type
+		opts.on('--db-type STRING', "Type of DB [postgresql, couchdb]") do |val|
+			options['db_type'] = val
 		end
 		
-		opts.on('--sql-port STRING', "PostgreSQL Port") do |val|
-			options['sql_port'] = val
+		
+		# SQL
+		opts.on('--postgresql-host STRING', "PostgreSQL Hostname") do |val|
+			options['postgresql_host'] = val
 		end
 		
-		opts.on('--sql-db STRING', "PostgreSQL Database Name") do |val|
-			options['sql_db'] = val
+		opts.on('--postgresql-port STRING', "PostgreSQL Port") do |val|
+			options['postgresql_port'] = val
 		end
 		
-		opts.on('--sql-user STRING', "PostgreSQL Username") do |val|
-			options['sql_user'] = val
+		opts.on('--postgresql-db STRING', "PostgreSQL Database Name") do |val|
+			options['postgresql_db'] = val
 		end
 		
-		opts.on('--sql-password STRING', "PostgreSQL Password") do |val|
-			options['sql_password'] = val
+		opts.on('--postgresql-user STRING', "PostgreSQL Username") do |val|
+			options['postgresql_user'] = val
 		end
 		
-		opts.on('--sql_host STRING', "PostgreSQL Hostname") do |val|
-			options['sql_host'] = val
+		opts.on('--postgresql-password STRING', "PostgreSQL Password") do |val|
+			options['postgresql_password'] = val
 		end
 
+		# CouchDB
+		opts.on('--couchdb-host STRING', "CouchDB Hostname") do |val|
+			options['couchdb_host'] = val
+		end
+		
+		opts.on('--couchdb-port STRING', "CouchDB Port") do |val|
+			options['couchdb_port'] = val
+		end
+		
+		opts.on('--couchdbl-db STRING', "CouchDB Database Name") do |val|
+			options['couchdb_db'] = val
+		end
+		
+		opts.on('--couchdb-user STRING', "CouchDB Username") do |val|
+			options['couchdb_user'] = val
+		end
+		
+		opts.on('--couchdb-password STRING', "CouchDB Password") do |val|
+			options['couchdb_password'] = val
+		end
+		
+		# Twitter
 		opts.on('--consumer-key STRING', "Twitter Consumer Key") do |val|
 			options['consumer_key'] = val
 		end
@@ -79,7 +120,7 @@ class TweetStreamApp < Bsl::Application
 	def main()
 		super()
 		
-		conn = PGconn.connect(options['sql_host'], options['sql_port'], '', '', options['sql_db'], options['sql_user'], options['sql_password'])
+		conn = PGconn.connect(options['postgresql_host'], options['postgresql_port'], '', '', options['postgresql_db'], options['postgresql_user'], options['postgresql_password'])
 		
 		TweetStream.configure do |config|
 			config.consumer_key = options['consumer_key']
